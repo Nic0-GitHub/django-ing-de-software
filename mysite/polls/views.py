c5794f2074f137c2ed0b8b96fe5f5f8eb3fa3f31
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-04-10 20:46:59 -0300

    [Parte 3.6] Agrego caso de Error 404 a la view detail de 'polls'

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -8,7 +9,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
