3443a92554630f1fbf75b8af9237859db1dc1668
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-04-15 18:45:51 -0300

    [Mejora 1.4] Añado redirect para index a 'polls'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='polls/', permanent=True)),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
